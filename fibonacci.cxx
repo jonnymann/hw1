8f3f8d0c6784ebf09541af7672e7029f2f5de98a
jonnymann <jonny-stracke@web.de>
2015-10-29 00:06:44 +0100

    Update fibonacci.cxx

--- fibonacci.cxx
+++ fibonacci.cxx
@@ -4,14 +4,15 @@ using namespace std;
 
 int main()
 {
-  N=10;
-  x1=0;
-  x2=1;
-  for(int i=1;i<N,i++)
+  int N=10;
+  int x1=0;
+  int x2=1;
+  int x3=1;
+  for(int i=1;i<N;i++)
   {
     x3=x2+x1;
-    x1=x2
-    x2=x3
+    x1=x2;
+    x2=x3;
   }
   cout<<x2;
   return 0;
